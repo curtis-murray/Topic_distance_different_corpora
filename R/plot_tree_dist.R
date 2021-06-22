b1249cba6c4eebf40e799a5eb833b3f1ef9a5476
Curtis Murray <murrcurt09@gmail.com>
2021-06-23 01:17:37 +0930

    i probably fucked up

---
+++ R/plot_tree_dist.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(tidytext)
+
+Vocab <- read_csv("data/clean_posts.csv") %>% 
+  unnest_tokens(word, Content) %>% 
+  group_by(word) %>%
+  summarise() %>% 
+  arrange(word) %>% 
+  mutate(word_ID_full = 1:n())
+
+unweighted_d <- list.files("data/Sampling_Problem/Distances", full.names = TRUE) %>% 
+  as_tibble() %>% 
+  select(path = value) %>% 
+  mutate(data = map(
+    path, ~read_csv(.x)
+  )
+  ) %>% 
+  unnest(data) %>% 
+  left_join(read_csv("data/Sampling_Problem/Samples.info/samples_info.csv"), by = "Sample") %>% 
+  select(-path) %>% 
+  arrange(Sample) %>% 
+  mutate(Unweighted = d/nrow(Vocab)^2) %>% 
+  select(Sample, Sample_prop, Unweighted)
+
+weighted_d <- list.files("data/Tree_Distance/", full.names = TRUE) %>% 
+  as_tibble() %>% 
+  select(path = value) %>% 
+  mutate(data = map(
+    path, ~read_csv(.x, col_names = c("Sample", "d"))
+  )
+  ) %>% 
+  unnest(data) %>% 
+  left_join(read_csv("data/Sampling_Problem/Samples.info/samples_info.csv"), by = "Sample") %>% 
+  select(-path) %>% 
+  arrange(Sample) %>% 
+  select(Sample, Sample_prop, Weighted = d)
+
+p <- full_join(unweighted_d, weighted_d, by = c("Sample", "Sample_prop")) %>% 
+  pivot_longer(names_to = "Distance", values_to = "d", cols = c(Unweighted, Weighted)) %>% 
+  ggplot() + 
+  geom_point(aes(x = Sample_prop, y = d, color = Distance), alpha = 0.3, show.legend = F) + 
+  geom_smooth(aes(x = Sample_prop, y = d, color = Distance), se = F) + 
+  labs(y = "Distance between Topic Structures",
+       x = "Sampling Proportion") + 
+  theme_minimal() + 
+  lims(y = c(0,3), x = c(0,1)) +
+  theme(legend.position = "bottom")
+
+
+p %>% ggsave(filename = "Figures/Plots/Sampling_Problem/distance.pdf", 
+             device = "pdf", 
+             height = 6, 
+             width=8)
