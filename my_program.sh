9a6caef910e16c947c26ac232a002165ae783115
Curtis Murray <murrcurt09@gmail.com>
2021-06-23 02:10:35 +0930

    fixing bugs

--- my_program.sh
+++ my_program.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm data/Samples/*
-rm data/Tidy_Topics/*
-rm data/Tree_Distance/*
 rm log
 
 wait
@@ -10,24 +7,24 @@ source n_sub
 remainder=n_sub%5
 wait
 t=0
-
+n_sub=13
 R CMD BATCH R/phoenix_gen_sample_info.R
 
 for i in $(seq 1 $(($n_sub/5)))
 do
     for j in $(seq 1 5)
     do
-    python3.9 Python/phoenix_hSBM.py $((($i-1)*5+$j-1)) &
+    python3.9 Python/phoenix_hSBM.py $((($i-1)*5+$j)) &
     now=$date
-    echo "Running sub $((($i-1)*5+$j-1)) at $now" >> log
+    echo "Running sub $((($i-1)*5+$j)) at $now" >> log
     done
     wait
 done
-for i in $(seq 0 $remainder)
+for i in $(seq 0 $(($remainder)))
 do
     python3.9 Python/phoenix_hSBM.py $(($n_sub-$i)) &
     now=date
-    echo "Running sub $i at $now" >> log
+    echo "Running sub $(($n_sub-$i)) at $now" >> log
 done
 wait
 R CMD BATCH R/phoenix_tidy.R
