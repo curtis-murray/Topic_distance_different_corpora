811cf931a6a03ca4401f7067ca85ff4201d2d55d
murrcurt09 <murrcurt09@gmail.com>
2021-06-30 01:14:25 +0930

    gitignore

--- R/plot_tree_dist.R
+++ R/plot_tree_dist.R
@@ -5,7 +5,12 @@ weighted_d <- list.files("data/Tree_Distance", full.names = TRUE) %>%
   as_tibble() %>% 
   select(path = value) %>% 
   mutate(data = map(
-    path, ~read_csv(.x, col_names = c("Sub1", "Sub2", "d"))
+    path, ~read_csv(.x, col_names = c("Sub1", 
+                                      "Sub2", 
+                                      "distance_unweighted", 
+                                      "distance_corpora_weighted", 
+                                      "distance_both_ave_weighted",
+                                      "distance_full_weighted"))
   )
   ) %>% 
   unnest(data)
@@ -13,57 +18,59 @@ weighted_d <- list.files("data/Tree_Distance", full.names = TRUE) %>%
 ids <- tibble(Sub = c(weighted_d %>% pull(Sub1), weighted_d %>% pull(Sub2)) %>% unique()) %>% 
   mutate(id = 1:n())
 
-
 weighted_d %>% 
   select(-path) %>% 
   left_join(ids, by = c("Sub1"="Sub")) %>% 
   left_join(ids, by = c("Sub2"="Sub")) ->
   edge_list
 
+for(dist in colnames(weighted_d)[4:7]){
+
 D = matrix(0, nrow(ids), nrow(ids))
-D[cbind(edge_list$id.x, edge_list$id.y)] = edge_list$d
+
+D[cbind(edge_list$id.x, edge_list$id.y)] = edge_list %>% pull(!!as.name(dist))
 
 D = D %>% t() + D
 rownames(D) <- ids$Sub
 colnames(D) <- ids$Sub
 
-write.table(D, file="../Meeting Reports/Meeting_Book/files/heatmap")
+write.table(D, file=paste("../Meeting Reports/Meeting_Book/files/heatmap_",dist,sep=""))
 
 library(superheat)
-
-pdf(file="Figures/heatmap.pdf",width = 14, height = 11, onefile = F)
-D %>% superheat(
-  #heat.lim = c(-1,1),
-  pretty.order.rows = T,
-  pretty.order.cols = T,
-  row.dendrogram = T,
-  clustering.method = "hierarchical",
-  #left.label.text.col = my_colouring$colour,
-  left.label.col = "white",
-  left.label.text.alignment = "left",
-  #bottom.label.text.col = my_colouring$colour,
-  bottom.label.col = "white",
-  bottom.label.text.angle = 90,
-  bottom.label.text.alignment = "right",
-  grid.hline.col = "white",
-  grid.hline.size = 0.5,
-  grid.vline.col = "white",
-  grid.vline.size = 0.5,
-  #yt.plot.type = "bar",
-  #yt = ordered_data$p,
-  #yt.axis.name = "Overall Density",
-  #heat.pal = c("blue", "white", "red"),
-  heat.pal = c("white", "red"),
-  left.label.text.size = 4,
-  bottom.label.text.size = 3.4,
-  yt.plot.size = 0.1
-)
-dev.off()
+# 
+# pdf(file=paste("Figures/heatmap",dist,".pdf",sep = ""),width = 14, height = 11, onefile = F)
+# D %>% superheat(
+#   #heat.lim = c(-1,1),
+#   pretty.order.rows = T,
+#   pretty.order.cols = T,
+#   row.dendrogram = T,
+#   clustering.method = "hierarchical",
+#   #left.label.text.col = my_colouring$colour,
+#   left.label.col = "white",
+#   left.label.text.alignment = "left",
+#   #bottom.label.text.col = my_colouring$colour,
+#   bottom.label.col = "white",
+#   bottom.label.text.angle = 90,
+#   bottom.label.text.alignment = "right",
+#   grid.hline.col = "white",
+#   grid.hline.size = 0.5,
+#   grid.vline.col = "white",
+#   grid.vline.size = 0.5,
+#   #yt.plot.type = "bar",
+#   #yt = ordered_data$p,
+#   #yt.axis.name = "Overall Density",
+#   #heat.pal = c("blue", "white", "red"),
+#   heat.pal = c("white", "red"),
+#   left.label.text.size = 4,
+#   bottom.label.text.size = 3.4,
+#   yt.plot.size = 0.1
+# )
+# dev.off()
 library(heatmaply)
 heatmaply(D,colors = c("white", "red"), grid_gap = 1, 
           dendrogram = "both",show_dendrogram = c(T,F), 
           revC=T,main = "Distances between subreddits",
-          file = "Figures/heatmap.html", dend_hoverinfo = F)
+          file = paste("Figures/heatmap_",dist,".html",sep = ""), dend_hoverinfo = F)
 
 posts <- read_csv("data/clean_posts.csv") %>% 
   group_by(Sub) %>% 
@@ -82,13 +89,13 @@ data_mds <- D %>% cmdscale() %>%
 p_mds <- data_mds %>% 
   ggplot() + 
   geom_point(aes(V1,V2,size = size),show.legend = F) + 
-  ggrepel::geom_label_repel(aes(V1,V2, label = sub)) + 
+  ggrepel::geom_label_repel(aes(V1,V2, label= sub), max_overlaps = 999) + 
   theme_void()
 
 p_mds
 
-p_mds %>% ggsave(filename = "Figures/p_mds.pdf",device = "pdf", width = 6, height = 4)
-p_mds %>% ggsave(filename = "../Meeting Reports/Meeting_Book/files/Figures/p_mds.png",device = "png", width = 6, height = 4)
+p_mds %>% ggsave(filename = paste("Figures/p_mds_",dist,".pdf",sep =""),device = "pdf", width = 6, height = 4)
+p_mds %>% ggsave(filename = paste("../Meeting Reports/Meeting_Book/files/Figures/p_mds_",dist,".png",sep = ""),device = "png", width = 6, height = 4)
 
 data_tsne <- D %>% tsne(perplexity = 2) %>% 
   as_tibble() %>%
@@ -98,13 +105,13 @@ data_tsne <- D %>% tsne(perplexity = 2) %>%
 p_tsne <- data_tsne %>% 
   ggplot() + 
   geom_point(aes(V1,V2,size = size),show.legend = F) + 
-  ggrepel::geom_label_repel(aes(V1,V2, label = sub)) + 
+  ggrepel::geom_label_repel(aes(V1,V2, label= sub), max_overlaps = 999) + 
   theme_void()
 
 p_tsne
 
-p_tsne %>% ggsave(filename = "Figures/p_tsne.pdf",device = "pdf", width = 6, height = 4)
-p_tsne %>% ggsave(filename = "../Meeting Reports/Meeting_Book/files/Figures/p_tsne.png",device = "png", width = 6, height = 4)
+p_tsne %>% ggsave(filename = paste("Figures/p_tsne_",dist,".pdf",sep =""),device = "pdf", width = 6, height = 4)
+p_tsne %>% ggsave(filename = paste("../Meeting Reports/Meeting_Book/files/Figures/p_tsne_",dist,".png",sep = ""),device = "png", width = 6, height = 4)
 
 
 custom.config = umap.defaults
@@ -118,10 +125,11 @@ data_umap <-  D %>% umap(config = custom.config) %>% .$layout %>%
 p_umap <- data_umap %>% 
   ggplot() + 
   geom_point(aes(V1,V2,size = size),show.legend = F) + 
-  ggrepel::geom_label_repel(aes(V1,V2, label = sub)) + 
+  ggrepel::geom_label_repel(aes(V1,V2, label= sub), max_overlaps = 999) + 
   theme_void() 
 
 p_umap
 
-p_umap %>% ggsave(filename = "Figures/p_umap.pdf",device = "pdf", width = 6, height = 4)
-p_umap %>% ggsave(filename = "../Meeting Reports/Meeting_Book/files/Figures/p_umap.png",device = "png", width = 6, height = 4)
+p_umap %>% ggsave(filename = paste("Figures/p_umap_",dist,".pdf",sep =""),device = "pdf", width = 6, height = 4)
+p_umap %>% ggsave(filename = paste("../Meeting Reports/Meeting_Book/files/Figures/p_umap_",dist,".png",sep = ""),device = "png", width = 6, height = 4)
+}
